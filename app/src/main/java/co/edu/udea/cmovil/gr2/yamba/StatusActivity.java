9715619f61251bfd059279d051344c5df2d62f7b
Daego <Daegoetx>
2015-04-09 03:54:25 -0500

    Chapter 7 clear

--- app/src/main/java/co/edu/udea/cmovil/gr2/yamba/StatusActivity.java
+++ app/src/main/java/co/edu/udea/cmovil/gr2/yamba/StatusActivity.java
@@ -1,17 +1,72 @@
 package co.edu.udea.cmovil.gr2.yamba;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.*;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
 
 
-public class StatusActivity extends ActionBarActivity {
+public class StatusActivity extends ActionBarActivity implements OnClickListener {
+
+    private static final String TAG = "StatusActivity";
+    private Button buttonTweet;
+    private EditText editStatus;
+    private TextView textCount;
+    private int defaultTextColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+
+        buttonTweet = (Button) findViewById(R.id.buttonTweet);
+        editStatus = (EditText) findViewById(R.id.editStatus);
+        textCount = (TextView) findViewById(R.id.textCount);
+        defaultTextColor = textCount.getTextColors().getDefaultColor();
+
+        buttonTweet.setOnClickListener(this);
+        editStatus.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int count = 140 - editStatus.length();
+                textCount.setText(Integer.toString(count));
+                if (count>10 && count<140){
+                    textCount.setTextColor(Color.GREEN);
+                }
+               else if(count<=10 && count >0){
+                    textCount.setTextColor(Color.YELLOW);
+                }
+                else if(count==0){
+                    textCount.setTextColor(Color.RED);
+                }
+                else{
+                    textCount.setTextColor(defaultTextColor);
+                }
+            }
+        });
+
+        defaultTextColor = textCount.getTextColors().getDefaultColor();
     }
 
 
@@ -35,5 +90,41 @@ public class StatusActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String status = editStatus.getText().toString();
+        Log.d(TAG,"nClicked with Status: " + status);
+
+        new PostTask().execute(status);
+    }
+
+    private final class PostTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            YambaClient yambaCloud = new YambaClient("student","password");
+            try{
+                yambaCloud.postStatus(params[0]);
+                return "Success on posting";
+            }
+            catch (YambaClientException e){
+                e.printStackTrace();
+                return "Failed on posting";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+        }
+
+        /*@Override
+        public boolean onCreateOptionsMenu(Menu menu){
+            getMenuInflater().inflate(R.menu.menu_status, menu);
+            return true;
+        }*/
     }
 }
